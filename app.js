15acd76eb1c209dc81ae2994da6bdf094f9ee775
VityaGoro <32596459+VityaGoro@users.noreply.github.com>
2018-03-20 22:49:18 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-const express = require('express');
-const Squares = require('./data/squares');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const app = express();
-const squares = new Squares();
+app.use(express.static('public')); //подключить статические файлы
+var router = require('./application/router/router');
+app.use(router); //исп-ть удаленный роутер
 
-app.use(express.static(__dirname + '/public'));
+var UserManager = require('./application/modules/user/userManager');
 
-app.get('/vm-21/areaSS', (req, res) => {
-    let A = req.query.A;
-    let B = req.query.B;
-    let F; eval('F = function (x) { return ' + req.query.f +'; }');
-    let G; eval('G = function (x) { return ' + req.query.g +'; }');
-    let method = req.query.method || 'прямоугольник';
-    if (!isNaN(A - 0) && !isNaN(B - 0) && F instanceof Function && G instanceof Function) {
-        try {
-            return res.send(squares.calc({a: A, b: B}, {f: F, g: G}, method));
-        } catch(e) {
-            res.send('err');
-        }
-    }
-    res.send('wrong params');
-});
+var SOCKET_EVENTS = {
+    USER_LOGIN: 'user login',
+    USER_LOGOUT: 'user logout',
+    USER_START_GAME: 'user start game'
+};
 
-app.get('/vm-21/:name/:soname', (req, res) => {
-    let name = req.params.name;
-    let soname = req.params.soname;
-    res.send('Hello, ' + ((name) ? name : 'unknown ') + ' ' + ((soname) ? soname : 'unknown ') + ' sosiska!!!');
-});
+var userManager = new UserManager({ io: io, SOCKET_EVENTS: SOCKET_EVENTS });
 
-app.all('/*', (req, res) => {
-    res.send('wrong way');
-});
-
-const server = app.listen(3000, () => {
+http.listen(3000, function () {
     console.log('server start at port 3000');
 });
